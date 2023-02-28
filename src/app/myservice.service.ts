2cc912a047e98b331147a832f62b3c0eab88055c
Wontat <98503728+Wontat@users.noreply.github.com>
2023-02-28 13:46:16 +0800

    3 files modified

--- src/app/myservice.service.ts
+++ src/app/myservice.service.ts
@@ -5,9 +5,9 @@ import { Injectable } from '@angular/core';
 })
 export class MyserviceService {
   constructor() {}
-  serviceproperty;
   showTodayDate() {
     let ndate = new Date();
     return ndate;
   }
+  serviceproperty = 'Service Created';
 }
